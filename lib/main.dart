4b11449bcf810e96224b159156bebd27237bd544
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-03-05 21:16:47 +0700

    Tab Bar Mantap Cutom Mulus

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  List<Tab> myTab = [
+    Tab(
+      text: "Tab 1",
+      icon: Icon(Icons.add_a_photo),
+    ),
+    Tab(
+      text: "Tab 2",
+      icon: Icon(Icons.ac_unit_outlined),
+
+    ),
+    Tab(
+      text: "Tab 3",
+      icon: Icon(Icons.access_alarm),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        initialIndex: 1,
+        length: myTab.length,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+            title: Text("My Apps"),
+            bottom: TabBar(
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.white,
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+              unselectedLabelStyle: TextStyle(
+                fontWeight: FontWeight.normal
+              ),
+              // indicatorColor: Colors.green,
+              // indicatorWeight: 5,
+              // indicatorPadding: EdgeInsets.all(10),
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicator: BoxDecoration(
+                color: Colors.amber,
+                // borderRadius: BorderRadius.circular(50)),\
+                border: Border(
+                  bottom : BorderSide(
+                    color: Colors.black, 
+                    width: 5,
+                ),
+              ),
+            ),
+            tabs: myTab,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text(
+                "Tab 1",
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Tab 2",
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Tab 3",
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            )
+        ],),
+      ),
+    ),
+    );
+  }
+}
